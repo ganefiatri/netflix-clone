d2b0247ae8ebfbf47b66b62bc4bab5742913ea84
ganefiatri <gicerf@gmail.com>
2022-11-15 04:59:56 +0700

    fix bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,20 +34,21 @@ const Home = ({
   const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
 
-  if(loading) return null
-  
+  if (loading) return null
+
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h=[140vh]">
+    <div className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${showModal && '!h-screen overflow-hidden'
+      }`}>
       <Head>
         <title>Home - Netflix Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Header */}
-      <Header/>
+      <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         {/* banner */}
-        <Banner netflixOriginals={netflixOriginals}/>
+        <Banner netflixOriginals={netflixOriginals} />
         <section className='md:space-y-24'>
           {/* Row */}
           <Row title="Trending Now" movies={trendingNow} />
@@ -89,7 +90,7 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ]) 
+  ])
 
   return {
     props: {
